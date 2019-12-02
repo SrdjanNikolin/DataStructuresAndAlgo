9d86644daac36a3c73413aae220bb1c57c87d4ee
Srdjan Nikolin <srdjannikolin@yahoo.com>
2019-12-02 13:05:46 +0100

    Added a method to print node values in order.

--- DataStructuresLibrary/Trees/BinaryTree/BinaryTree.cs
+++ DataStructuresLibrary/Trees/BinaryTree/BinaryTree.cs
@@ -15,5 +15,32 @@ namespace DataStructuresLibrary.Trees.BinaryTree
         {
             Root = new Node(data);
         }
+
+        public void Insert(int data)
+        {
+            if (Root == null)
+            {
+                Root = new Node(data);
+                return;
+            }
+        }
+        //Print Nodes Inorder Traversal
+        public void PrintInOrder(Node root)
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Tree is empty!");
+                return;
+            }
+            if (root.left != null)
+            {
+                PrintInOrder(root.left);
+            }
+            Console.WriteLine(root.Data);
+            if (root.right != null)
+            {
+                PrintInOrder(root.right);
+            }
+        }
     }
 }
