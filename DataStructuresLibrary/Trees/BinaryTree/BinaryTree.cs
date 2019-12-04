f86b500da1174541c19db9c5e3ea4f77030370c5
SrdjanNikolin <srdjannikolin@yahoo.com>
2019-12-04 20:57:21 +0100

    Added methods for printing pre-order and post-order as well as inserting a new node. Removed unnecessary usings.

--- DataStructuresLibrary/Trees/BinaryTree/BinaryTree.cs
+++ DataStructuresLibrary/Trees/BinaryTree/BinaryTree.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DataStructuresLibrary.Trees.BinaryTree
 {
@@ -24,7 +22,7 @@ namespace DataStructuresLibrary.Trees.BinaryTree
                 return;
             }
         }
-        //Print Nodes Inorder Traversal
+        //Print Nodes In-order Traversal
         public void PrintInOrder(Node root)
         {
             if (root == null)
@@ -40,6 +38,73 @@ namespace DataStructuresLibrary.Trees.BinaryTree
             if (root.right != null)
             {
                 PrintInOrder(root.right);
+            }
+        }
+        //Print Nodes Pre-order Traversal
+        public void PrintPreOrder(Node root)
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Tree is empty!");
+            }
+            Console.WriteLine(root.Data);
+            if (root.left != null)
+            {
+                PrintPreOrder(root.left);
+            }
+            if (root.right != null)
+            {
+                PrintPreOrder(root.right);
+            }
+        }
+        //Print Nodes Post-order Traversal
+        public void PrintPostOrder(Node root)
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Tree is empty!");
+            }
+            if (root.left != null)
+            {
+                PrintPostOrder(root.left);
+            }
+            if (root.right != null)
+            {
+                PrintPostOrder(root.right);
+            }
+            Console.WriteLine(root.Data);
+        }
+        /*Insert Node in Binary Tree, note that this implementation is not good if it was
+         a binary search tree, as it does not check the root value of subtrees. 
+         This method will simply insert a node on the right side if it's value is bigger, else insert at left.*/
+        public void InsertInOrder(Node root, Node newNode)
+        {
+            if (root == null)
+            {
+                root = newNode;
+                return;
+            }
+
+            if (newNode.Data > root.Data)
+            {
+                if (root.right != null)
+                {
+                    InsertInOrder(root.right, newNode);
+                }
+                else
+                {
+                    root.right = newNode;
+                    return;
+                }
+            }
+            else if (newNode.Data < root.Data)
+            {
+                if (root.left != null)
+                {
+                    InsertInOrder(root.left, newNode);
+                }
+                root.left = newNode;
+                return;
             }
         }
     }
